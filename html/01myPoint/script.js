24f8a76ca708c60d77a1e2e9c5a4f0c14166fecb
Calvin Davidson <29150@ma-web.nl>
2020-02-24 11:12:54 +0100

    first commit

---
+++ html/01myPoint/script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+
+let p = new Point(new Vector2d(50, 50), 10, "red");
+p.draw();
