6f3628793bbd0c45b3b748c2424b3d5dc8d8f734
Calvin Davidson <29150@ma-web.nl>
2020-02-24 12:17:13 +0100

    added wall collision, and velocity

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -9,9 +9,42 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
+let circles = [];
+
+
 function newCircle() {
-let p = new Point(new Vector2d(getRandomInt(0, width), getRandomInt(0, height)), getRandomInt(2, 50), getRandomColor());
-p.draw(context);
+    if (circles.length <= 50) {
+        let p = new Point(new Vector2d(getRandomInt(0, width), getRandomInt(0, height)), getRandomInt(10, 50), getRandomColor(), getRandomInt(2, 5));
+        p.draw(context);
+
+        circles.push(p);
+    }
 }
 
 setInterval(newCircle, 10);
+
+
+
+
+
+
+function Update() {
+context.clearRect(0, 0, width, height);
+
+    for(let i = 0; i < circles.length; i++) {
+        let newX = circles[i].pos.dx + circles[i].VelX;
+        let newY = circles[i].pos.dy + circles[i].VelY;
+
+        circles[i].updatePos(new Vector2d(newX, newY));
+        circles[i].draw(context);
+        circles[i].wallCollision(width, height);
+    
+        // collision with walls
+        
+    }
+}
+
+setInterval(Update, 10);
+
+
+
